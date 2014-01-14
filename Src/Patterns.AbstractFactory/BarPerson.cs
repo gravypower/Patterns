77a413b19a2e75a1c8f7b41eb0a6b2be66a713f7
Aaron <aaron@gravypower.net>
2014-01-14 15:03:23 +0000

    Removed an unused method

--- Src/Patterns.AbstractFactory/BarPerson.cs
+++ Src/Patterns.AbstractFactory/BarPerson.cs
@@ -6,15 +6,13 @@
     /// <summary>
     /// The bar person.
     /// </summary>
-    /// <typeparam name="T">
-    /// The factory that supplies the drink to me made.
-    /// </typeparam>
     public class BarPerson : IBarPerson 
     {
         /// <summary>
         /// The mix drink.
         /// </summary>
         /// <typeparam name="T">
+        /// The factory that supplies the drink to me made.
         /// </typeparam>
         /// <returns>
         /// The <see cref="IDrink"/>.
@@ -22,11 +20,6 @@
         public IDrink MixDrink<T>() where T : ICocktailFactory, new()
         {
             return new T().FavoriteDrink();
-        }
-
-        public IDrink MixDrink()
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
